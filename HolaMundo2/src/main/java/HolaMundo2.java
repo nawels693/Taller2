072e7a9e0d7534b50e616077eb6ed7402e861a62
Nawel <n.huenchuleo01@ufromail.cl>
2021-07-14 01:40:28 -0400

    prueba

---
+++ HolaMundo2/src/main/java/HolaMundo2.java
@@ -0,0 +1,7 @@
+public class HolaMundo2 {
+    public static void main(String[] args) {
+        System.out.println("hola mundo2");
+    }
+}
+
+
---
+++ Prueba3/src/main/java/Prueba3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Prueba3 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+          System.out.println("opcion 1");
+        System.out.println("opcion 2");
+int opcion = teclado.nextInt();
+        switch (opcion){
+            case 1:
+                break;
+            case 2:
+                break;
+        }
+
+    }
+}
